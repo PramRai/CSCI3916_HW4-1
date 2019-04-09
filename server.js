e3081c356aa299ee3e2215d2a7cc796d4efbee02
Oleksiy Pobyeda <oleksiypobyeda@gmail.com>
2019-04-09 01:14:47 -0600

    gj

--- server.js
+++ server.js
@@ -163,7 +163,7 @@ router.route('/movie/:movieid')
                     if (err) {
                         res.json({message: "Error accured.", error: err});
                     } else {
-                        res.json({movie: brandNewVar, message:"Here is what I found:"});
+                        res.json({brandNewVar: brandNewVar, message:"Here is what I found:"});
                     }
                 })
             } else {
