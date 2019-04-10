2d3f130aadf4705499f1b408854da34d370ef867
Oleksiy Pobyeda <oleksiypobyeda@gmail.com>
2019-04-09 19:47:22 -0600

    sad

--- server.js
+++ server.js
@@ -244,6 +244,7 @@ router.route('/review')
         var id = req.body.movieid;
         Movie.findById(id, function (err, something){
             if (err) {
+                res.json("Movie not exist!");
                 res.json({message: "Error 🚨"});
             }
             if (something) {
