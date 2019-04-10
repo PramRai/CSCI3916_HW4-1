afebed0db971f65e187fcbac911c33df1e3e0c44
Oleksiy Pobyeda <oleksiypobyeda@gmail.com>
2019-04-09 23:44:50 -0600

    asdasd

--- server.js
+++ server.js
@@ -150,7 +150,7 @@ router.route('/movie/:movieid')
             else {
                 if (needReview == "true"){
 
-                    Review.find({$movieid : id}, function (err, rev){
+                    Review.findOne({movieid : id}, function (err, rev){
                         if (err) {
                             res.json({message: "Error .", error: err});
                         } else {
